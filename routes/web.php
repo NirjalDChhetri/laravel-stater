a02f1f9a0710a0f611902cfeaec8b3fafc1309b7
NirjalDChhetri <nirjald3@gmail.com>
2023-07-03 17:46:00 +0545

    login and registration

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
-use App\Http\Controllers\FileController;
-use App\Http\Controllers\StudentController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,20 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('contact', function () {
-    return view('contact');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-//FileUpload
-Route::get('file-upload', [FileController::class, 'index']);
-Route::post('file-upload', [FileController::class, 'store'])->name('files.store');
-
-//Database connection
-
-Route::get('database', function () {
-    return view('database');
-});
-
-// student routes
-
-Route::resource("students", StudentController::class);
+require __DIR__.'/auth.php';
